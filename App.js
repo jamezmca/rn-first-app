87ef1b6f8ded1e39dbff201ccda28c134d8c41b4
James McArthur <jamesmca@live.com>
2021-02-12 14:45:16 -0800

    Adding text input feature and button in flexbox layout

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{padding: 50}}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+        <TextInput placeholder="Course Goal" 
+        style={{ width: '80%', borderColor: 'black', borderWidth: 1, padding: 10}} 
+        />
+        <Button title="ADD"/>
+      </View>
+      <View>
+
+      </View>
+      
+      
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ 
 });
+ 
