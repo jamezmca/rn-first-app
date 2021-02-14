d3f68c40ee88f4e55e5369ba113f33ba0eadab5d
James McArthur <jamesmca@live.com>
2021-02-13 22:45:21 -0800

    changing to style sheet
    instead of inline styling

--- App.js
+++ App.js
@@ -3,16 +3,25 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
-  
+  const [enteredGoal, setEnteredGoal] = useState('');
 
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
 
+  const addGoalHandler = () => {
+    console.log(enteredGoal);
+  };
   return (
-    <View style={{padding: 50}}>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-        <TextInput placeholder="Course Goal" 
-        style={{ width: '80%', borderColor: 'black', borderWidth: 1, padding: 10}} 
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput 
+          placeholder="Course Goal" 
+          style={styles.input} 
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
         />
-        <Button title="ADD"/>
+        <Button title="ADD" onPress={addGoalHandler}/>
       </View>
       <View>
 
@@ -25,6 +34,19 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
- 
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+     flexDirection: 'row',
+     justifyContent: 'space-between', 
+     alignItems: 'center'
+  },
+  input: {
+    width: '80%', 
+    borderColor: 'black', 
+    borderWidth: 1, 
+    padding: 10
+  }
 });
  
