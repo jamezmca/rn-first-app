0ab67ab8f5c34cdcc6bf9f6cb5bfd8e536fe5737
James McArthur <jamesmca@live.com>
2021-02-17 20:08:47 -0800

    created components to react native main file
    Still need to go through and add in ID's
    
    components handle input functionality and data storage

--- App.js
+++ App.js
@@ -1,33 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView,  FlatList} from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState('');
+  
+  const [courseGoals, setCourseGoals] = useState([]);
 
-  const goalInputHandler = (enteredText) => {
-    setEnteredGoal(enteredText);
+
+  const addGoalHandler = goalTitle => {
+    setCourseGoals(currentGoals => [...courseGoals, { key: Math.random().toString(), value: goalTitle }
+    ]);
   };
 
-  const addGoalHandler = () => {
-    console.log(enteredGoal);
-  };
+
   return (
     <View style={styles.screen}>
-      <View style={styles.inputContainer}>
-        <TextInput 
-          placeholder="Course Goal" 
-          style={styles.input} 
-          onChangeText={goalInputHandler}
-          value={enteredGoal}
-        />
-        <Button title="ADD" onPress={addGoalHandler}/>
-      </View>
-      <View>
+      <GoalInput onAddGoal={addGoalHandler}/>
+      <FlatList 
+        data={courseGoals} 
+        renderItem={ItemData => <GoalItem title={ItemData.item.value}/>}
+      />
 
-      </View>
-      
-      
       <StatusBar style="auto" />
     </View>
   );
@@ -37,16 +32,7 @@ const styles = StyleSheet.create({
   screen: {
     padding: 50
   },
-  inputContainer: {
-     flexDirection: 'row',
-     justifyContent: 'space-between', 
-     alignItems: 'center'
-  },
-  input: {
-    width: '80%', 
-    borderColor: 'black', 
-    borderWidth: 1, 
-    padding: 10
-  }
+
+
 });
  
